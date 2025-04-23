1baf77edda7d278f83dc14a4037335813aa981d8
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2025-04-23 19:53:06 -0400

    initial commit: only dolphin currently

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "KDE-platform apps compiled for Darwin";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-24.11-darwin";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = { flake-parts, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } ({ self, ... }: {
+    flake.overlays.default = import ./overlay.nix;
+
+    systems = inputs.nixpkgs.lib.platforms.darwin;
+    perSystem = { pkgs, self', system, ... }: {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [ self.overlays.default ];
+      };
+
+      packages = {
+        dolphin = pkgs.kdePackages.dolphin;
+      };
+
+      apps = {
+        type = "app";
+        program = self'.packages.dolphin;
+      };
+    };
+  });
+}
---
+++ overlay.nix
@@ -0,0 +1,93 @@
+final: prev: prev.lib.optionalAttrs prev.stdenv.isDarwin {
+  kdePackages = prev.kdePackages.overrideScope (kfinal: kprev:
+  let
+    meta-platform-darwin = pkg: pkg.overrideAttrs (oldAttrs: {
+      meta = oldAttrs.meta // {
+        platforms = prev.lib.platforms.darwin;
+      };
+    });
+
+    is-darwin = pkg:
+    (!builtins.isNull pkg) &&
+    (builtins.hasAttr "meta" pkg) &&
+    (builtins.hasAttr "platforms" pkg.meta) &&
+    (builtins.any (plat: builtins.elem plat prev.lib.platforms.darwin) pkg.meta.platforms);
+
+    remove-non-darwin = pkgs: builtins.filter is-darwin pkgs;
+
+    name-matches = pkg: name:
+    (!builtins.isNull pkg) &&
+    (builtins.hasAttr "pname" pkg) &&
+    (pkg.pname == name);
+
+    remove-all-names = to-remove: from: builtins.filter (pkg: !builtins.any (name: name-matches pkg name) to-remove) from;
+    purge = pkgs: to-remove: remove-all-names to-remove (remove-non-darwin pkgs);
+
+    to-darwin = pkg: (meta-platform-darwin pkg).overrideAttrs (oldAttrs: {
+      buildInputs = (purge oldAttrs.buildInputs [
+        "acl"
+        "qtwayland"
+      ]) ++ [
+        kfinal.extra-cmake-modules
+      ];
+      nativebuildInputs = oldAttrs.nativeBuildInputs ++ [
+        kprev.wrapQtAppsHook
+      ];
+      propagatedUserEnvPkgs = prev.lib.optionals (builtins.hasAttr "propagatedUserEnvPkgs" oldAttrs)
+      (remove-non-darwin oldAttrs.propagatedUserEnvPkgs);
+      propagatedBuildInputs = purge oldAttrs.propagatedBuildInputs [
+        "packagekit-qt"
+      ];
+    });
+  in {
+    dolphin = (to-darwin kprev.dolphin).overrideAttrs (oldAttrs: {
+      buildInputs = oldAttrs.buildInputs ++ [
+        kprev.qtsvg
+      ];
+    });
+
+    kcmutils = to-darwin kprev.kcmutils;
+    knewstuff = to-darwin kprev.knewstuff;
+    kcoreaddons = to-darwin kprev.kcoreaddons;
+    ki18n = to-darwin kprev.ki18n;
+    kdbusaddons = to-darwin kprev.kdbusaddons;
+    kbookmarks = to-darwin kprev.kbookmarks;
+    kconfig = to-darwin kprev.kconfig;
+    kio = to-darwin kprev.kio;
+    kparts = to-darwin kprev.kparts;
+    solid = to-darwin kprev.solid;
+    kiconthemes = to-darwin kprev.kiconthemes;
+    kcompletion = to-darwin kprev.kcompletion;
+    ktextwidgets = to-darwin kprev.ktextwidgets;
+    knotifications = to-darwin kprev.knotifications;
+    kcrash = to-darwin kprev.kcrash;
+    kwindowsystem = to-darwin kprev.kwindowsystem;
+    kwidgetsaddons = to-darwin kprev.kwidgetsaddons;
+    kcodecs = to-darwin kprev.kcodecs;
+    kguiaddons = to-darwin kprev.kguiaddons;
+
+    karchive = (to-darwin kprev.karchive).overrideAttrs (oldAttrs: {
+      buildInputs = oldAttrs.buildInputs ++ [ prev.bzip2 ];
+    });
+    kcolorscheme = to-darwin kprev.kcolorscheme;
+    breeze-icons = to-darwin kprev.breeze-icons;
+    sonnet = to-darwin kprev.sonnet;
+    kservice = to-darwin kprev.kservice;
+    kitemviews = to-darwin kprev.kitemviews;
+    kjobwidgets = to-darwin kprev.kjobwidgets;
+    kauth = to-darwin kprev.kauth;
+    kconfigwidgets = to-darwin kprev.kconfigwidgets;
+    kxmlgui = to-darwin kprev.kxmlgui;
+    kpackage = to-darwin kprev.kpackage;
+    attica = to-darwin kprev.attica;
+    phonon = (to-darwin kprev.phonon).overrideAttrs (oldAttrs: {
+      buildInputs = purge oldAttrs.buildInputs [
+        "libpulseaudio"
+      ];
+    });
+
+    extra-cmake-modules = meta-platform-darwin kprev.extra-cmake-modules;
+  });
+  # TODO: This has to be a bug in nix?
+  # extra-cmake-modules = final.kdePackages.extra-cmake-modules;
+}
